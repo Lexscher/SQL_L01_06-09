abdd5e625b56a95b32e77cd595c4aa254e61b2d0
Alexander Schelchere <ascellie@gmail.com>
2019-06-09 11:33:52 -0400

    updated schema

--- 01_create_cats_table.sql
+++ 01_create_cats_table.sql
@@ -1,5 +1,7 @@
 CREATE TABLE cats (
-  id INTEGER PRIMARY KEY,
-    name TEXT, 
-    age INTEGER
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    age INTEGER,
+    breed TEXT,
+    owner_id INTEGER
 );
